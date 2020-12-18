41a8315f0c266975e0d44c47612b556e16c44599
Tanya Khatcherian <tanyakhatcherian@Tanyas-MacBook-Pro.local>
2020-12-18 15:43:16 -0500

    eigth-commit

--- js/script.js
+++ js/script.js
@@ -15,6 +15,8 @@ let audio = null // empty for now
 
 //playing songs
 let playASong = function(whichSong) {
+    prev.toggleAttribute(`disabled`, true)
+	next.toggleAttribute(`disabled`, true)
 	let song = playlist[whichSong] 
 	if (audio) { 
 		audio.src = song.src // Change the song **
@@ -25,19 +27,42 @@ let playASong = function(whichSong) {
     
     if (i == 0) {
         songTitle.textContent = `DREAMS`
-        document.body.style.background = "url('https://images.unsplash.com/photo-1499346030926-9a72daac6c63?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1650&q=80') no-repeat"
+        document.body.classList.add(`swap`)
+        setTimeout(()=>{ 
+	                document.body.style.backgroundImage = `url('https://images.unsplash.com/photo-1499346030926-9a72daac6c63?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1650&q=80')` 
+                                }, 500)
+        //document.body.style.background = "url('https://images.unsplash.com/photo-1499346030926-9a72daac6c63?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1650&q=80') no-repeat"
     } else if (i == 1) {
         songTitle.textContent = `OCEAN WAVE`
-        document.body.style.background = "url('https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1653&q=80')"
+        document.body.classList.add(`swap`)
+        setTimeout(()=>{ 
+	                document.body.style.backgroundImage = `url('https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1653&q=80')` 
+                                }, 500)
+        //document.body.style.background = "url('https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1653&q=80')"
     } else if (i == 2) {
         songTitle.textContent = `RAINFALL`
-        document.body.style.background = "url('https://i.pinimg.com/originals/71/a9/5d/71a95d3e30320ee8f62696a485e87aba.jpg')"
+        document.body.classList.add(`swap`)
+        setTimeout(()=>{ 
+	                document.body.style.backgroundImage = `url('https://i.pinimg.com/originals/71/a9/5d/71a95d3e30320ee8f62696a485e87aba.jpg')` 
+                                }, 500)
+        //document.body.style.background = "url('https://i.pinimg.com/originals/71/a9/5d/71a95d3e30320ee8f62696a485e87aba.jpg')"
     } 
-    // Rocco help me, because I know if you continue clicking next, i goes up by one, 
-    //but i only have three songs so i tried doing i === 3 no song, but it doesn't work, 
-    // i need to know how to fix the scenario of i being < 0 and > 2 and having the audio just 
-    //stop which i could maybe do audio.load?
-    //and my other thing is how do i transition the background photos with like a fade-in?
+
+    document.body.addEventListener('animationend', () => {
+        document.body.classList.remove(`swap`)
+        
+        if (i <= 0) {
+            i = 0
+        } else {
+            previous.toggleAttribute(`disabled`, false)
+        }
+        
+        if (i >= imgs.length-1) {
+            i = imgs.length-1
+        } else {
+            next.toggleAttribute(`disabled`, false)
+        }
+    })
 
 }
 
@@ -62,9 +87,7 @@ let pressedNext = function() {
     if((i + 1) < trackCount){
         i = i + 1
         playASong(i)
-    } else {
-        audio.pause();
-    }
+    } 
 }
 
 let next = document.querySelector(`#next`)
@@ -75,9 +98,7 @@ let pressedPrev = function() {
     if((i - 1) > -1){
         i = i - 1
         playASong(i)
-    } else {
-        audio.pause()
-    }
+    } 
 }
 
 let previous = document.querySelector(`#prev`)
